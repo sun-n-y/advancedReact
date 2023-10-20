edf78a366d50bb30b95b6935706b2163651a8b27
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-20 16:27:34 -0400

    add use state w/ object batching

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,22 +1,28 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  const [name, setName] = useState('peter');
-  const [age, setAge] = useState(24);
-  const [hobby, setHobby] = useState('read books');
+  // gorup them
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    hobby: 'read books',
+  });
+
+  // const [name, setName] = useState('peter');
+  // const [age, setAge] = useState(24);
+  // const [hobby, setHobby] = useState('read books');
 
   const handleClick = () => {
-    setName('john');
-    setAge(44);
-    setHobby('soccer');
+    // setPerson({ name: 'john', age: 25, hobby: 'scream at the computer' });
+    setPerson({ ...person, hobby: 'scream at the computer' });
   };
 
   return (
     <>
       <h2>useState object example</h2>
-      <h4>{name}</h4>
-      <h4>{age}</h4>
-      <h4>Enjoys : {hobby}</h4>
+      <h4>{person.name}</h4>
+      <h4>{person.age}</h4>
+      <h4>Enjoys : {person.hobby}</h4>
       <button type="button" className="btn" onClick={handleClick}>
         show john
       </button>
