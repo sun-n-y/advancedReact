6e3121ff77e40ab6aadd5a329417b3d5df8e9cd4
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-20 12:40:58 -0400

    add use state basics

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // console.log(useState(1));
+  // const value = useState('hello')[0];
+  // const func = useState('hello')[1];
+  // console.log(value);
+  // console.log(func);
+
+  const [count, setCount] = useState(0);
+  console.log(count);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h4>you clicked {count}</h4>
+      <button className="btn" type="button" onClick={handleClick}>
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
