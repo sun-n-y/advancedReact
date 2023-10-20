e129314f04d3cb6beca2eff4029df160e4f9813e
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-20 16:37:03 -0400

    add set function gotcha

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+  console.log(count);
+  return (
+    <>
+      <h2>useState "gotcha"</h2>
+      <h1>{count}</h1>
+      <button type="button" onClick={increase} className="btn">
+        increase
+      </button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
