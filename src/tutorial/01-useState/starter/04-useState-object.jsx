5e5ec38ea604879d6e46247739db1205534f9538
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-20 16:11:51 -0400

    add use state w/ object

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,27 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState('peter');
+  const [age, setAge] = useState(24);
+  const [hobby, setHobby] = useState('read books');
+
+  const handleClick = () => {
+    setName('john');
+    setAge(44);
+    setHobby('soccer');
+  };
+
+  return (
+    <>
+      <h2>useState object example</h2>
+      <h4>{name}</h4>
+      <h4>{age}</h4>
+      <h4>Enjoys : {hobby}</h4>
+      <button type="button" className="btn" onClick={handleClick}>
+        show john
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
