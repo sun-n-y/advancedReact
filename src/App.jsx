b1fad8ee798cb601c9bc8ba281f8938bd62a09bb
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-23 15:50:12 -0400

    add short circuit lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
+import Starter from './tutorial/03-conditional-rendering/starter/04-short-circuit-overview';
 
 function App() {
   return (
--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,18 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  //falsy
+  const [text, setText] = useState('');
+  //truthy
+  const [name, setName] = useState('susan');
+
+  return (
+    <div>
+      <h4>falsy or :{text || 'hello world'}</h4>
+      <h4>falsy and :{text && 'hello world'}</h4>
+      <h4>falsy or :{name || 'hello world'}</h4>
+      <h4>truthy and :{name && 'hello world'}</h4>
+    </div>
+  );
 };
 export default ShortCircuitOverview;
