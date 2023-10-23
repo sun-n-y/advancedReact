e5931cfd78733e3b85b2907257b73eb5875c15dd
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-23 11:15:59 -0400

    add use effect basics 1

--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -3,10 +3,14 @@ import { useState } from 'react';
 const CodeExample = () => {
   const [value, setValue] = useState(0);
 
+  const sayHello = () => {
+    console.log('hello there');
+  };
+  sayHello();
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
