83122c06231d5eddd278c0d54114bbb4740080a2
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-23 16:34:31 -0400

    add conditional rendering toggle challenge

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,24 @@
+import { useState } from 'react';
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [value, setValue] = useState(false);
+
+  // const handleClick = () => {
+  //   if (value) {
+  //     setValue(false);
+  //     return;
+  //   }
+  //   setValue(true);
+  // };
+
+  return (
+    <div>
+      <button className="btn" onClick={() => setValue(!value)}>
+        click me
+      </button>
+      {value && <h1>:D</h1>}
+    </div>
+  );
 };
 
 export default ToggleChallenge;
