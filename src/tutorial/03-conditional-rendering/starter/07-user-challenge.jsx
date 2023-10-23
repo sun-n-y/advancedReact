2dfd7a6999fa4ba26a4895f8b53f92bfe184a7a6
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-23 16:54:54 -0400

    add conditional rendering user challenge

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    setUser({ name: 'j' });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>hello there: {user.name}</h4>
+          <button onClick={logout}>logout</button>
+        </div>
+      ) : (
+        <div>
+          <h4>please login </h4>
+          <button onClick={login}>login</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
