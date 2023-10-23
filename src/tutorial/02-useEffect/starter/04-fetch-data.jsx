7efd19a3cd878bfff76aa4d3a3cbb71d0b3363e9
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-23 12:21:40 -0400

    add use effect fetch data checkpoint

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,41 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const data = await response.json();
+          setUsers(data);
+        } else {
+          throw new Error('wrong url');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <>
+      {users.map((user) => {
+        const { id, avatar_url: image, login: name, url: link } = user;
+        return (
+          <div key={id}>
+            <img src={image} style={{ height: '75px', width: '75px' }} alt="" />
+            <h4>{name}</h4>
+            <a href={link}>profile</a>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 export default FetchData;
