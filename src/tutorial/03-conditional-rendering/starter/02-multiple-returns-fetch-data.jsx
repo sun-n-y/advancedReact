652aa86bf87a06b265881aeee623f84d4a751b07
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-23 14:23:10 -0400

    add conditional rendering challenge 1

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,44 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setError] = useState(false);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        setError(true);
+        console.log(error);
+      }
+      setIsLoading(false);
+    };
+    getUser();
+  }, []);
+
+  if (isLoading) {
+    return <h2>loading....</h2>;
+  }
+
+  if (isError) {
+    <h2>error...</h2>;
+  }
+
+  return (
+    <div>
+      <img
+        style={{ width: '150px', borderRadius: '25px' }}
+        src={user.avatar_url}
+        alt={user.name}
+      />
+      <h2>{user.name}</h2>
+      <h4>{user.company}</h4>
+      <p>{user.bio}</p>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
