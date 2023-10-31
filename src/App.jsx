a9feaa914c9db72b85d6d68e3a3fe40d82592d1c
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-31 16:36:57 -0400

    add glint and code spell checker

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Test } from './Test';
 import Example from './tutorial/04-project-structure/starter/Example';
 
 function App() {
   return (
     <div className="container">
       <Example />
+      <Test />
     </div>
   );
 }
---
+++ src/Test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export function Test({}) {
+  return (
+    <div>
+      <h3>hello world</h3>
+    </div>
+  );
+}
