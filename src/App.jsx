45fe2da27eec7ab1e123a97935f6f14af657d0c5
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-31 16:27:10 -0400

    add group of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Example from './tutorial/04-project-structure/starter/Example';
+
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Example />
+    </div>
+  );
 }
 
 export default App;
