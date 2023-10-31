e7d5d85094b3f7551f057ff9030ab333c0166ba3
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-31 16:10:30 -0400

    add project structure for component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
+import Navbar from './tutorial/04-project-structure/starter/Navbar';
 
 function App() {
   return (
     <div className="container">
-      <Starter />
+      <Navbar />
     </div>
   );
 }
