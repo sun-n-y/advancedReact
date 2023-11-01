59d42f8d16cbb61875df57f6789d02f75edfd837
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-01 11:20:09 -0400

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import List from './tutorial/05-leverage-javascript/starter/List';
+import Starter from './tutorial/06-forms/starter/01-controlled-inputs';
 
 function App() {
   return (
     <div className="container">
-      <List />
+      <Starter />
     </div>
   );
 }
--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,23 @@
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  return (
+    <form className="form">
+      <h4>controlled inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input type="text" name="" id="name" className="form-input" />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input type="text" name="" id="email" className="form-input" />
+      </div>
+      <button type="submit" className="btn btn-block">
+        submit
+      </button>
+    </form>
+  );
 };
 export default ControlledInputs;
