b7efc550a93f0b603ddb5995a81de8eb52457e7c
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-01 12:41:55 -0400

    add users and remove

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,56 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+
 const UserChallenge = () => {
+  const [name, setName] = useState('');
+  const [users, setUsers] = useState(data);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) return;
+    const newUser = { id: Math.random(), name };
+    const updatedUsers = [...users, newUser];
+    setUsers(updatedUsers);
+    setName('');
+  };
+
+  const removeUser = (id) => {
+    const updateUsers = users.filter((user) => user.id !== id);
+    setUsers(updateUsers);
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       {/* render users below */}
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <button type="button" onClick={() => removeUser(user.id)}>
+              remove
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
