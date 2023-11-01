7a1ccb322aaa97125f7ccf8b02a7f2b3497e222e
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-01 11:41:26 -0400

    add controlled inputs

--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,18 +1,44 @@
+import { useState } from 'react';
+
 const ControlledInputs = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email);
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <h4>controlled inputs</h4>
       <div className="form-row">
         <label htmlFor="name" className="form-label">
           name
         </label>
-        <input type="text" name="" id="name" className="form-input" />
+        <input
+          type="text"
+          name=""
+          id="name"
+          className="form-input"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
       </div>
       <div className="form-row">
         <label htmlFor="email" className="form-label">
           email
         </label>
-        <input type="text" name="" id="email" className="form-input" />
+        <input
+          type="text"
+          name=""
+          id="email"
+          className="form-input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <button type="submit" className="btn btn-block">
         submit
