81c2977eb600155219a8d041c72c44d80b8c28df
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-15 11:13:41 -0500

    add useeffect to rememdy problem

--- src/tutorial/11-performance/starter/01-lower-state/Person.jsx
+++ src/tutorial/11-performance/starter/01-lower-state/Person.jsx
@@ -2,9 +2,9 @@ import { useEffect } from 'react';
 
 const Person = ({ name }) => {
   console.log('render');
-  // useEffect(() => {
-  //   console.log('unfortunately does not fix the issue');
-  // }, []);
+  useEffect(() => {
+    console.log('unfortunately does not fix the issue');
+  }, []);
   return (
     <div>
       <h4>{name}</h4>
